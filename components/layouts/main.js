5ec5bee0d1a864a3dfd12a58448f85a604c15d05
Angel Zúñiga Navarro <angel.2112b@gmail.com>
2022-09-16 17:24:11 +0000

    Added the side bar to main / Created the side bar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import NavBar from "../navBar";
+import SideBar from "../sideBar";
 import { Box, Container } from "@chakra-ui/react";
 const Main = (props) => {
   const { children, router, title } = props;
@@ -12,6 +13,7 @@ const Main = (props) => {
         <title>{title}</title>
       </Head>
       <NavBar path={router.asPath} />
+      <SideBar pt={14}/>
       <Container maxW="container.md" pt={14}>
         {children}
       </Container>
