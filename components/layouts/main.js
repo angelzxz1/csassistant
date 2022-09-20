ecc07e8e268cb5f9997182b52cc9002758d85af3
Angel Zúñiga Navarro <69635320+angelzxz1@users.noreply.github.com>
2022-09-20 18:11:43 -0500

    Edited the main to have the grid more stable

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -2,49 +2,38 @@ import Head from "next/head";
 import NavBar from "../navBar";
 import SideBar from "../sideBar";
 import { Box, Container, Grid, GridItem } from "@chakra-ui/react";
-const Main = props => {
-	const { children, router, title } = props;
-	return (
-		<Box as="main" pb={8}>
-			<Head>
-				<meta charSet="utf-8" />
-				<meta
-					name="viewport"
-					content="initial-scale=1.0, width=device-width"
-				/>
-				<link
-					rel="shortcut icon"
-					href="/favicon.ico"
-					type="image/x-icon"
-				/>
-				<title>{title}</title>
-			</Head>
-			<NavBar path={router.asPath} />
-			<SideBar pt={14} display={{ base: "none", lg: "block" }} w="20%" />
-			<Container maxW="container.md" pt={14}>
-				<Grid
-					templateAreas={{
-						base: `"sidebar children children"
+const Main = (props) => {
+  const { children, router, title } = props;
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <title>{title}</title>
+      </Head>
+      <NavBar path={router.asPath} />
+      <SideBar pt={14} display={{ base: "none", lg: "block" }} w="20%" />
+      <Container maxW="container.md" pt={14}>
+        <Grid
+          templateAreas={{
+            base: `"sidebar children children"
           "sidebar children children"
           "sidebar children children"
           `,
-						lg: `"sidebar"`,
-					}}
-					templateColumns={{ base: "1fr 2fr", lg: "1fr" }}
-					templateRows={{ base: "1fr 1fr 1fr", lg: "1fr" }}
-				>
-					<GridItem gridArea="sidebar">
-						<SideBar
-							pt={14}
-							display={{ base: "block", lg: "none" }}
-							w="100%"
-						/>
-					</GridItem>
-					<GridItem gridArea="children">{children}</GridItem>
-				</Grid>
-			</Container>
-		</Box>
-	);
+            lg: `"children"`,
+          }}
+          templateColumns={{ base: "25% 75%", lg: "100%" }}
+          templateRows="3fr"
+        >
+          <GridItem gridArea={{ base: "sidebar", lg: "" }}>
+            <SideBar pt={14} display={{ base: "block", lg: "none" }} />
+          </GridItem>
+          <GridItem gridArea="children">{children}</GridItem>
+        </Grid>
+      </Container>
+    </Box>
+  );
 };
 
 export default Main;
