5ee0311e1f74e740f4e24d9251d37bff6ef7ad19
angelzxz1 <angel.zuna@icloud.com>
2022-09-24 00:44:23 -0500

    no me acuerdo que es xd

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,15 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Main from "../components/layouts/main";
 import theme from "../lib/theme";
 import Fonts from "../components/fonts";
-const WebSite = (props) => {
-  const { Component, pageProps, router } = props;
-  return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      <Main router={router} title={Component.tittle}>
-        <Component {...pageProps} />
-      </Main>
-    </ChakraProvider>
-  );
+const WebSite = props => {
+	const { Component, pageProps, router } = props;
+	return (
+		<ChakraProvider theme={theme}>
+			<Fonts />
+			<Main router={router} title={Component.tittle}>
+				<Component {...pageProps} />
+			</Main>
+		</ChakraProvider>
+	);
 };
 export default WebSite;
